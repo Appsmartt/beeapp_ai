8ee2ed20ef01787c6e7fb644abcbf94ddb38f799
GordoSaurio <andres.santa-fe@hotmail.com>
2026-08-20 11:32:16 -0500

    update tunnel

--- Backend/beeAppBack/beeAppBack/settings.py
+++ Backend/beeAppBack/beeAppBack/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
     "192.168.1.5",
-    "sustained-portland-walt-chi.trycloudflare.com",
+    "practitioner-jenny-featured-wiki.trycloudflare.com",
 ]
 
 INSTALLED_APPS = [
